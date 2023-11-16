4b96a8bc4295cb523b93b1b00506b66d86b11337
Cheniqua Breaux <cbreaux2@mkc02dq300md6r.humad.com>
2023-11-16 09:49:03 -0500

    for real this time

---
+++ script.js
@@ -0,0 +1,54 @@
+fetch("https://api.punkapi.com/v2/beers/random")
+.then((response) => {
+    const details = response.json()
+    return details
+})
+.then((details) =>{
+    console.log(details)
+    const container = document.getElementById("container")
+   
+
+    for(i = 0; i < details.length; i++){
+
+        headerContainer = document.createElement("div")
+        descriptionContainer = document.createElement("div")
+        imageContainer = document.createElement("div")
+        tagContainer = document.createElement("div")
+
+
+        const header = document.createElement("h1")
+        const description = document.createElement("p")
+        const image = document.createElement("img")
+        const tag = document.createElement("p")
+
+        container.appendChild(headerContainer)
+        container.appendChild(descriptionContainer)
+        container.appendChild(imageContainer)
+        container.appendChild(tagContainer)
+        
+        header.innerText = details[i].name
+        description.innerText = details[i].description
+        image.src = details[i].image_url
+        tag.innerText = details[i].tagline
+        
+        headerContainer.classList.add("header")
+        descriptionContainer.classList.add("details")
+        imageContainer.classList.add("image")
+        tagContainer.classList.add("tagline")
+        
+        headerContainer.appendChild(header)
+        descriptionContainer.appendChild(description)
+        imageContainer.appendChild(image)
+        tagContainer.appendChild(tag)
+    }
+})
+
+
+
+
+
+
+
+.catch((error) =>{
+    console.log(error)
+})
